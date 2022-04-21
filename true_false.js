c2032f5ae8d3d57699f9c8499702ae290eed0102
Mitul <md.mitul.hossen99@gmail.com>
2022-04-21 23:24:24 +0600

    true and false concept

---
+++ true_false.js
@@ -0,0 +1,26 @@
+//Truthy
+//n=any number(without 0)
+//n="any string";
+//n=" ";
+//n="0";
+//n={};
+//n=[];
+//n="false";
+
+//Falsy
+//n=0;
+//n="";
+//n=undefined;
+//n=null;
+//n=NaN;
+//n=false;
+
+
+const n = false;
+if(n){
+      console.log("Condition is True");
+}
+
+else{
+      console.log("Condition is False");
+}
