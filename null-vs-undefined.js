d91a20ab60c64c3aa8c0266526c764615a99c072
Mitul <md.mitul.hossen99@gmail.com>
2022-04-21 23:33:58 +0600

    null vs undefined concept

---
+++ null-vs-undefined.js
@@ -0,0 +1,16 @@
+
+
+//const n;
+
+      //console.log(n);
+
+function add(n1,n2){
+      console.log(n1+n2);
+}
+const addNum =add(3,4);
+
+console.log(addNum);
+
+//const n=undefined;
+
+      //console.log(n);
