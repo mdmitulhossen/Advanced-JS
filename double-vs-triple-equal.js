ba6803c75292e190f6c55d14b33b742addc96f17
Mitul <md.mitul.hossen99@gmail.com>
2022-04-21 23:40:39 +0600

    ==vs=== concept

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,22 @@
+//== check value
+
+//===check value and type
+
+// const n = 2;
+// const m= "2";
+
+// if(n===m){
+//       console.log("condition true");
+// }
+// else{
+//       console.log("condition false");
+// }
+const n = 1;
+const m= true;
+
+          if(n===m){
+               console.log("condition true");
+            }
+            else{
+               console.log("condition false");
+            }
